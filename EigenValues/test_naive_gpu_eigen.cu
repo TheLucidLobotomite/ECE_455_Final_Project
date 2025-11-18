fb57389d76e1ea7674cc92aa5ccae0fdecf95b69
Payton <pmsmith5@wisc.edu>
2025-11-17 20:52:23 -0600

    New test of 1000x1000

--- EigenValues/test_naive_gpu_eigen.cu
+++ EigenValues/test_naive_gpu_eigen.cu
@@ -64,8 +64,8 @@ void free_matrix(double** mat, int n) {
 int main() {
     printf("\n=== GPU EIGENVALUE SOLVER BENCHMARK ===\n\n");
 
-    const int n = 100;
-    const int num_runs = 10000;
+    const int n = 1000;
+    const int num_runs = 1000;
     const double sparsity = 0.01;
     double* times_ms = (double*)malloc(num_runs * sizeof(double));
 
